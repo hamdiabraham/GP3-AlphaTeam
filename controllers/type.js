24c303d92c47035407ad69b915292f30de30cf1b
Ferraridiaz <87638147+Ferraridiaz@users.noreply.github.com>
2021-09-30 16:23:52 +0700

    membuat create pada bagian types_room

---
+++ controllers/type.js
@@ -0,0 +1,35 @@
+const type = require("../models").Type;
+
+class Type {
+    static async makeType(req,res) {
+        const {is_include_breakfast, type_name, price} = req.body;
+        if (!is_include_breakfast || !type_name || !price) {
+            res.status(400).json({
+                message: "is_include_breakfast, type, price must be fill"
+            });
+        } else {
+            const isTypeNameExist = await type.findOne({
+                where: {
+                    type_name: type_name
+                }
+            });
+            if (isTypeNameExist) {
+                res.status(409).json({
+                    message: "type name is exist"
+                });
+            } else {
+                const types = await type.create({
+                    is_include_breakfast: is_include_breakfast,
+                    type_name: type_name,
+                    price: price
+                });
+                res.status(201).json({
+                    message: "success create type room",
+                    types
+                });
+            }
+        }
+    }
+}
+
+module.exports = Type;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const authRouter = require("./auth");
+const authentication = require("../midllewares/authentication");
 const mainRouter = require("express").Router();
+const typeRouter = require("./type");
+
 
 mainRouter.use(authRouter);
+mainRouter.use(typeRouter);
+// mainRouter.use(authentication);
 
 module.exports = mainRouter;
