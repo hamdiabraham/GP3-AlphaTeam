7327b0a0893c4900426f786e62830dd5be571ff4
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-01 21:25:12 +0700

    Membuat error handler dan menerapkannya di auth js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const mainRouter = require("./routes");
-
 const express = require("express");
 const app = express();
 const port = 5000;
+const handlerError = require("./middlewares/handlerError");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(mainRouter);
+
+app.use(handlerError);
 
 app.listen(port, () => console.log(`Running server on port ${port}`));
--- controllers/auth.js
+++ controllers/auth.js
@@ -3,79 +3,67 @@ const jwt = require("jsonwebtoken");
 const users = require("../models").User;
 
 class Auth {
-  static async login(req, res) {
+  static async login(req, res, next) {
     const { email, password } = req.body;
-
     if (!email || !password) {
-      res.status(400).json({
-        message: "email and password must be fill",
+      next({ code: 415, message: "email and password must be fill" });
+    } else if (!password.length >= 8 && !password.length <= 16) {
+      next({
+        code: 415,
+        message: "password min 8 character and max 16 character"
       });
-    }
-
-    if (password.length >= 8 && password.length <= 16) {
-      if (/[\w\d]+@[\w]+\.[\w]+/.test(email)) {
-        const user = await users.findOne({
-          where: {
-            email: email,
-          },
-        });
-        if (user != null && bcrypt.compareSync(password, user.password)) {
-          const token = jwt.sign({ id: user.id }, "secure");
-          res.status(200).json({
-            message: "success login",
-            token,
-          });
-        } else {
-          res.status(401).json({
-            message: "fail login",
-          });
+    } else if (!/[\w\d]+@[\w]+\.[\w]+/.test(email)) {
+      next({ code: 415, message: "invalid email" });
+    } else {
+      const user = await users.findOne({
+        where: {
+          email: email
         }
+      });
+      if (user == null || !bcrypt.compareSync(password, user.password)) {
+        next({ code: 401, message: "email or password is wrong" });
       } else {
-        res.status(400).json({
-          message: "invalid email",
+        const token = jwt.sign({ id: user.id }, "secure");
+        res.status(200).json({
+          message: "success login",
+          token
         });
       }
-    } else {
-      res.status(400).json({
-        message: "password min 8 character and max 16 character",
-      });
     }
   }
 
   static async register(req, res) {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
-      res.status(400).json({
-        message: "name, email, dan password must be fill",
-      });
+      next({ code: 415, message: "name, email, and password must be fill" });
     } else if (!/[\w\d]+@[\w]+\.[\w]+/.test(email)) {
-      res.status(400).json({
-        message: "invalid email",
-      });
+      next({ code: 415, message: "invalid email" });
     } else if (!(password.length >= 8 && password.length <= 16)) {
-      res.status(400).json({
-        message: "password min 8 character and max 16 character",
+      next({
+        code: 415,
+        message: "password min 8 character and max 16 character"
       });
     } else {
       const user = await users.findOne({
         where: {
-          email: email,
-        },
+          email: email
+        }
       });
       if (user) {
-        res.status(409).json({
-          message: "user already exist",
+        next({
+          code: 409,
+          message: "user already exist"
         });
       } else {
         const user = await users.create({
           name,
           email,
-          password: bcrypt.hashSync(password),
+          password: bcrypt.hashSync(password)
         });
         const token = jwt.sign({ id: user.id }, "secure");
         res.status(201).json({
           message: "success register",
-          token,
+          token
         });
       }
     }
