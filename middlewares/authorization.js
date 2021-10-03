1e9e1f2ca81373c0fb2ddd5bb6dcc330eaf74db8
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-03 12:16:52 +0700

    Membuat handle error pada authentication dan authorization

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,25 +1,21 @@
 const authorization = (req, res, next) => {
-  try {
-    const currentUser = req.currentUser;
-    const endPoint = req.url;
+  const currentUser = req.currentUser;
+  const endPoint = req.url;
 
-    if (
-      (new RegExp("/room").test(endPoint) ||
-        new RegExp("/type-room").test(endPoint) ||
-        new RegExp("/reservation").test(endPoint)) &&
-      !currentUser.is_guest
-    ) {
-      next();
-    } else if (
-      new RegExp("/reservation").test(endPoint) &&
-      currentUser.is_guest
-    ) {
-      next();
-    } else {
-      throw new Error("unauthorize access");
-    }
-  } catch (error) {
-    next(error);
+  if (
+    (new RegExp("/room").test(endPoint) ||
+      new RegExp("/type-room").test(endPoint) ||
+      new RegExp("/reservation").test(endPoint)) &&
+    !currentUser.is_guest
+  ) {
+    next();
+  } else if (
+    new RegExp("/reservation").test(endPoint) &&
+    currentUser.is_guest
+  ) {
+    next();
+  } else {
+    next({ code: 403, message: "unauthorize access" });
   }
 };
 
